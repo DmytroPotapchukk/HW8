8a7c9af433dcefa2e0239526d1a31c124f7a4089
DmytroPotapchukk <dmytropotapchukk@gmail.com>
2023-11-02 21:59:50 +0200

    first_commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from datetime import date, datetime
+
+weekdays = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Monday',
+    6: 'Monday',}
+
+
+def get_birthdays_per_week(users):
+    # Реалізуйте тут домашнє завдання
+    days = {}
+    current_date = date.today()
+    if len(users) == 0:
+        return {}
+    for birthdays in users:
+        birth_date = (birthdays.get('birthday'))
+        birth_date = datetime(year=current_date.year + 1, month=birth_date.month,
+                              day=birth_date.day).date()  # next year
+        difference = birth_date - current_date
+        if 0 <= int(difference.days) <= 6:
+            if days.get(weekdays.get(birth_date.weekday())):
+                days.get(weekdays.get(birth_date.weekday())).append(
+                    birthdays.get('name'))  # if exist
+            else:
+                days.update({str(weekdays.get(birth_date.weekday())): [
+                            birthdays.get('name')]})  # if new
+
+        birth_date = datetime(year=current_date.year, month=birth_date.month,
+                              day=birth_date.day).date()  # this year
+        difference = birth_date - current_date
+        if 0 <= int(difference.days) <= 6:
+            if days.get(weekdays.get(birth_date.weekday())):
+                days.get(weekdays.get(birth_date.weekday())
+                         ).append(birthdays.get('name'))
+            else:
+                days.update(
+                    {str(weekdays.get(birth_date.weekday())): [birthdays.get('name')]})
+
+    return days
+    # return users
+
+
+if __name__ == "__main__":
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+    ]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    # Виводимо результат
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
